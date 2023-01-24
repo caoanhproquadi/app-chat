21e518455372ecfa622a33f773ce67eec81cbf01
caoanhproquadi <dangcaoanh1998@gmail.com>
2023-01-24 18:39:32 +0700

    add all files chatApp

---
+++ test1.js
@@ -0,0 +1,11 @@
+let arr=[1,2,3,4]
+ arr.forEach((num, index) => {
+    return index= num * 2;
+});
+console.log(arr)
+
+let arr1=[1,2,3,4]
+let test =arr1.map(num => {
+    return num * 2;
+});
+console.log(test)
